4352d9793c1225dd31e2097219c512baec4f9c55
Daniel Delgado Bernini <danidb2901@hotmail.com>
2021-10-10 18:42:24 -0600

    Fist

---
+++ cmake-build-debug/CMakeFiles/Proyecto1Pr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1Pr.dir/main.cpp.obj"
+  "Proyecto1Pr.exe"
+  "Proyecto1Pr.exe.manifest"
+  "Proyecto1Pr.pdb"
+  "libProyecto1Pr.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1Pr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
